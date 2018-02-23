26e0dd2cec8d06644fd774242bbfd068b35deed9
Andrey Degtyaruk <hlogeon1@gmail.com>
2018-02-24 02:41:18 +0800

    Adopt to the latest reqiurements

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,21 +1,21 @@
 var SafeMath = artifacts.require('./SafeMath.sol');
 var StarCoin = artifacts.require("./StarCoin.sol");
 var StarCoinPreSale = artifacts.require("./StarCoinPreSale.sol");
+var InvestorWhiteList = artifacts.require("./InvestorWhiteList.sol");
 
-module.exports = function(deployer) {
+module.exports = async function(deployer) {
   deployer.deploy(SafeMath);
   deployer.link(SafeMath, StarCoin);
   deployer.link(SafeMath, StarCoinPreSale);
-  deployer.deploy(StarCoin).then(function() {
-    const hardCap = 12916;
-    const softCap = 0;
+  deployer.deploy(StarCoin).then(async function() {
+    await deployer.deploy(InvestorWhiteList);
     const token = StarCoin.address;
-    const totalTokens = 6328760; //NOT in wei, converted by contract
-    const limit = 4000; //in ETH
+    const investorWhiteList = InvestorWhiteList.address;
+    const hardCap = 632860;
+    const softCap = 0;
     const beneficiary = web3.eth.accounts[0];
     const startBlock = web3.eth.blockNumber;
     const endBlock = web3.eth.blockNumber + 100;
-
-    deployer.deploy(StarCoinPreSale, hardCap, softCap, token, beneficiary, totalTokens, limit, startBlock, endBlock);
+    deployer.deploy(StarCoinPreSale, hardCap, softCap, token, beneficiary, investorWhiteList, startBlock, endBlock);
   });
 };
